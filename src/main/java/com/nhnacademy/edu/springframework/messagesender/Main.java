223e6a737428790ee9548abb28bebbf6b4eab7cc
장준영 <jjy0372@gmail.com>
2022-10-11 15:38:29 +0900

    constructor dependency injection

--- src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
@@ -11,19 +11,22 @@ public class Main {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("classpath:/beans.xml");
 
-        User user = new User("hello", "010");
+//        User user = new User("hello", "010");
+//
+//        String message = "what";
+//
+//        System.out.println("-------------");
+//        context.getBean("sms", MessageSender.class).sendMessage(user, message);
+//        context.getBean("sms", MessageSender.class).sendMessage(user, message);
+//        System.out.println("-------------");
+//        context.getBean("email", MessageSender.class).sendMessage(user, message);
+//        context.getBean("email", MessageSender.class).sendMessage(user, message);
+//        System.out.println("-------------");
+//
+//        context.close();
 
-        String message = "what";
-
-        System.out.println("-------------");
-        context.getBean("sms", MessageSender.class).sendMessage(user, message);
-        context.getBean("sms", MessageSender.class).sendMessage(user, message);
-        System.out.println("-------------");
-        context.getBean("email", MessageSender.class).sendMessage(user, message);
-        context.getBean("email", MessageSender.class).sendMessage(user, message);
-        System.out.println("-------------");
-
-        context.close();
+        MessageSendService service = context.getBean("messagesendservice", MessageSendService.class);
+        service.doSendMessage();
 
     }
 
