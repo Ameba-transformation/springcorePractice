860026a08a7caf5085cac0f312812aa6ac872621
장준영 <jjy0372@gmail.com>
2022-10-11 15:53:04 +0900

    constructor dependency injection using 2constructor

--- src/main/java/com/nhnacademy/edu/springframework/messagesender/service/MessageSendService.java
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/service/MessageSendService.java
@@ -4,13 +4,20 @@ import com.nhnacademy.edu.springframework.messagesender.User;
 
 public class MessageSendService {
 
-    private final MessageSender messageSender;
+    private final MessageSender messageSender1;
+    private final MessageSender messageSender2;
 
-    public MessageSendService(MessageSender messageSender) {
-        this.messageSender = messageSender;
+//    public MessageSendService(MessageSender messageSender) {
+//        this.messageSender = messageSender;
+//    }
+
+    public MessageSendService(MessageSender emailMessageSender, MessageSender smsMessageSender) {
+        this.messageSender1 = emailMessageSender;
+        this.messageSender2 = smsMessageSender;
     }
 
     public void doSendMessage(){
-        messageSender.sendMessage(new User("abcde", "010-1234-5678"),"hello");
+        messageSender1.sendMessage(new User("abcde", "010-1234-5678"),"hello");
+        messageSender2.sendMessage(new User("abcde", "010-1234-5678"),"hello");
     }
 }
