d5fceb67799c8cd3f5286c16a5b75dd252ff2b08
장준영 <jjy0372@gmail.com>
2022-10-11 16:17:43 +0900

    setter dependency injection

--- src/main/java/com/nhnacademy/edu/springframework/messagesender/service/MessageSendService.java
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/service/MessageSendService.java
@@ -4,20 +4,22 @@ import com.nhnacademy.edu.springframework.messagesender.User;
 
 public class MessageSendService {
 
-    private final MessageSender messageSender1;
-    private final MessageSender messageSender2;
+    private MessageSender messageSender;
 
-//    public MessageSendService(MessageSender messageSender) {
-//        this.messageSender = messageSender;
-//    }
+    public MessageSendService(){
 
-    public MessageSendService(MessageSender emailMessageSender, MessageSender smsMessageSender) {
-        this.messageSender1 = emailMessageSender;
-        this.messageSender2 = smsMessageSender;
+    }
+
+    public void setMessageSender(MessageSender messageSender) {
+        System.out.println("setMessageSend!!!!");
+        this.messageSender = messageSender;
+    }
+
+    public MessageSendService(MessageSender messageSender) {
+        this.messageSender = messageSender;
     }
 
     public void doSendMessage(){
-        messageSender1.sendMessage(new User("abcde", "010-1234-5678"),"hello");
-        messageSender2.sendMessage(new User("abcde", "010-1234-5678"),"hello");
+        messageSender.sendMessage(new User("abcde", "010-1234-5678"),"hello");
     }
 }
